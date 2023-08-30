1d9ab52502845547a46eb0d5e12f750871a68556
JCHlearn <143541853+JCHlearn@users.noreply.github.com>
2023-08-30 12:39:03 -0400

    Update streamlit_app.py
    separated the code because of error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.subtitle('Breakfast Menu')
-streamlit.line('Omega 3 & Blueberry Oatmeal
-               Kale, Spinach & Rocket Smoothie
-               Hard-Boiled Free-Range Egg')
+
+streamlit.line('Omega 3 & Blueberry Oatmeal')
+
+streamlit.line('Kale, Spinach & Rocket Smoothie')
+
+streamlit.line ('Hard-Boiled Free-Range Egg')
