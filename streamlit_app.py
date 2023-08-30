ba377d99c4be2aac66093ea9039f2dfeed94b751
JCHlearn <143541853+JCHlearn@users.noreply.github.com>
2023-08-30 12:32:42 -0400

    Update streamlit_app.py
    added the menu title and the 3 lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.subtitle('Breakfast Menu')
+streamlit.line('Omega 3 & Blueberry Oatmeal
+               Kale, Spinach & Rocket Smoothie
+               Hard-Boiled Free-Range Egg')
